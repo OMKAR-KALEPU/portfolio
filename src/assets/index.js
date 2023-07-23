cee73eed18d39c9a2d176706a1b5e2444c667a16
OMKAR-KALEPU <omkar.kalepu2002@gmail.com>
2023-07-23 19:44:22 +0530

    Assets

---
+++ src/assets/index.js
@@ -0,0 +1,68 @@
+import logo from "./logo.png";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import instagram from "./instagram.png";
+import linkedIn from "./linkedIn.png";
+import twitter from "./twitter.png";
+
+import css from "./tech/css.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import mongodb from "./tech/mongodb.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import redux from "./tech/redux.png";
+import mui from "./tech/mui.png";
+import tailwind from "./tech/tailwind.png";
+import githubDark from "./tech/github-dark.png";
+import postman from "./tech/postman.png";
+import java from "./tech/java.png";
+import sql from "./tech/sql.png";
+
+import zyxan from "./company/zyxan.png";
+import gssoc from "./company/gssoc.png";
+import ssoc from "./company/ssoc.jpg";
+
+import currencyDetector from "./projects/currency_predictor.png";
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  instagram,
+  twitter,
+  linkedIn,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  git,
+  githubDark,
+  html,
+  postman,
+  javascript,
+  mongodb,
+  nodejs,
+  reactjs,
+  redux,
+  java,
+  sql,
+  mui,
+  tailwind,
+  zyxan,
+  gssoc,
+  ssoc,
+  currencyDetector,
+  carrent,
+  jobit,
+  tripguide,
+};
