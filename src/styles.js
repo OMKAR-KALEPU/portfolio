24f1c27b6fbe07c2772add80feba48823198fba4
OMKAR-KALEPU <omkar.kalepu2002@gmail.com>
2023-07-26 22:51:24 +0530

    Styles

--- src/styles.js
+++ src/styles.js
@@ -4,14 +4,14 @@ const styles = {
   padding: "sm:px-16 px-6 sm:py-10 py-7",
 
   heroHeadText:
-    "font-black text-white lg:text-[65px] sm:text-[60px] xs:text-[50px] text-[40px] lg:leading-[98px] mt-2",
+    "font-black text-white lg:text-[60px] sm:text-[45px] xs:text-[30px] text-[40px] lg:leading-[98px] mt-2",
   heroSubText:
-    "text-[#dfd9ff] font-medium lg:text-[25px] sm:text-[26px] xs:text-[20px] text-[14px] lg:leading-[40px]",
+    "text-[#dfd9ff] font-medium lg:text-[23px] sm:text-[20px] xs:text-[16px] text-[14px] lg:leading-[40px]",
 
   sectionHeadText:
-    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px]",
+    "text-white font-black md:text-[60px] sm:text-[50px] xs:text-[40px] text-[30px] text-center",
   sectionSubText:
-    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider",
+    "sm:text-[18px] text-[14px] text-secondary uppercase tracking-wider text-center",
 };
 
 export { styles };
